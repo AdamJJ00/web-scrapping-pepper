d6f4517ebbb7c3813b505dfd87e291be498085a4
jzmujdzin <jzmujdzin@gmail.com>
2023-04-16 12:49:48 +0200

    rework prices

--- pepper_recognition.py
+++ pepper_recognition.py
@@ -44,27 +44,20 @@ class PepperScrapper:
         html_content = self.web_driver.page_source
         self.current_soup = BS(html_content, "html.parser")
 
-    def scan_and_add_current_page_for_content(self) -> None:
+    def scan_current_page_for_content(self) -> None:
+        # TODO: po jakims <!-- buffered --> sie zaczyna psuc wszystko w dol - naprawic
         self.scrapped_data = pd.concat(
             [
                 self.scrapped_data,
                 pd.DataFrame(
                     [
                         self.extract_content_from_article(article)
-                        for article in self.current_soup.find_all(
-                            self.get_properties_for_dividing_page_to_articles()
-                        )
+                        for article in self.current_soup.find_all("article")
                     ],
                     columns=self.get_columns_for_content_df(),
                 ),
             ]
         )
-
-    @staticmethod
-    def get_properties_for_dividing_page_to_articles() -> tuple:
-        return "article", {
-            "class": "thread cept-thread-item thread--type-list imgFrame-container--scale thread--deal"
-        }
 
     def extract_content_from_article(self, article: Tag) -> list:
         content_div = article.find("div", {"class": "threadGrid thread-clickRoot"})
@@ -73,10 +66,13 @@ class PepperScrapper:
             content_div = article.find(
                 "div", {"class": "thread-fullMode threadGrid thread-clickRoot"}
             )
+        if content_div is None:
+            content_div = article.find("div", {"class": ""})
+        title, link = self.extract_title(content_div)
+        # print(title)
         hottness = self.extract_hottness(content_div)
         price_before_discount, price_after_discount = self.extract_prices(content_div)
         comments = self.extract_comments(content_div)
-        title, link = self.extract_title(content_div)
         username = self.extract_username(content_div)
         return [
             title,
@@ -91,30 +87,37 @@ class PepperScrapper:
     @staticmethod
     def extract_hottness(bargain_div: Tag) -> int:
         # there are 3 types of hottness: hot, burning and expired
+        # cept-vote-box vote-box overflow--hidden border border--color-borderGrey bRad--a thread-noClick
         hottness_tag = bargain_div.find(
-            "span", {"class": "cept-vote-temp vote-temp vote-temp--hot"}
+            "div",
+            {
+                "class": "cept-vote-box vote-box overflow--hidden border border--color-borderGrey bRad--a thread-noClick"
+            },
         )
         # case 1 - bargain is "hot" and we can extract the text easily
-        if hottness_tag and (
-            "hot" in hottness_tag["class"] or "vote-temp--hot" in hottness_tag["class"]
-        ):
+        if hottness_tag:
             hottness_extracted = hottness_tag.get_text()
-        # case 2 - bargain is "burning" and we can access the text straight from text
-        if not hottness_tag:
+        # # case 2 - bargain is "burning" and we can access the text straight from text
+        # if not hottness_tag:
+        #     hottness_tag = bargain_div.find(
+        #         "span", {"class": "cept-vote-temp vote-temp vote-temp--burn"}
+        #     )
+        #     hottness_extracted = str(hottness_tag)
+        # # case 3 - bargain is expired and we can access the hottness by other div tag
+        else:
             hottness_tag = bargain_div.find(
-                "span", {"class": "cept-vote-temp vote-temp vote-temp--burn"}
+                "div",
+                {
+                    "class": "vote-box vote-box--muted space--h-2 border border--color-borderGrey bRad--a text--color-grey space--mr-3"
+                },
             )
-            hottness_extracted = str(hottness_tag)
-        # case 3 - bargain is expired and we can access the hottness by other div tag
-        if not hottness_tag:
-            hottness_tag = bargain_div.find("span", {"class": "space--h-2 text--b"})
             hottness_extracted = hottness_tag.get_text()
-        hottness = (
-            re.search(r"\d+", hottness_extracted).group()
-            if hottness_extracted is not None
-            else -1
-        )
-        return int(hottness)
+        try:
+            hottness = re.search(r"\d+", hottness_extracted).group()
+        except AttributeError:
+            hottness = -1
+            # print(bargain_div)
+        return hottness
 
     @staticmethod
     def extract_username(bargain_div: Tag) -> str:
@@ -130,24 +133,23 @@ class PepperScrapper:
         # try to extract prices: sometimes, the price before discount tag is not present
         # there are also situations where there are no prices at all
         # and users just upload a bargain for other users to explore on their own
+        # try to find price after discount - if it is not present, then is is just a bargain
+        # or a link to a webpage for other users to explore on their own
         try:
-            # try to find price after discount - if it is not present, then is is just a bargain
-            # or a link to a webpage for other users to explore on their own
             price_after_discount = prices.find(
                 "span",
-                {"class": "thread-price text--b cept-tp size--all-l size--fromW3-xl"},
+                {"class": "overflow--wrap-off"},
             ).get_text()
-            price_before_discount_tag = prices.find(
-                "span",
-                {"class": "mute--text text--lineThrough size--all-l size--fromW3-xl"},
-            )
-            # if price_before_discount_tag is None, then there is no price before discount
-            if price_before_discount_tag is not None:
-                price_before_discount = price_before_discount_tag.get_text()
-            else:
-                price_before_discount = "price not available"
         except AttributeError:
             price_after_discount = "price not available"
+        price_before_discount_tag = prices.find(
+            "span",
+            {"class": "flex--inline boxAlign-ai--all-c space--ml-2"},
+        )
+        # if price_before_discount_tag is None, then there is no price before discount
+        try:
+            price_before_discount = price_before_discount_tag.find("span").get_text()
+        except AttributeError:
             price_before_discount = "price not available"
         return price_before_discount, price_after_discount
 
@@ -174,7 +176,7 @@ class PepperScrapper:
             self.current_page += 1
             self.web_driver.get(self.get_url_for_page(self.current_page))
             self.derive_html_content_from_driver_to_soup()
-            self.scan_and_add_current_page_for_content()
+            self.scan_current_page_for_content()
 
     @staticmethod
     def get_url_for_page(page_num: int):
@@ -185,5 +187,5 @@ if __name__ == "__main__":
     driver = webdriver.Chrome()
     driver.get("https://www.pepper.pl")
     scrapper = PepperScrapper(driver=driver)
-    scrapper.scan_and_add_current_page_for_content()
-    # scrapper.scan_next_pages()
+    scrapper.scan_current_page_for_content()
+    scrapper.scan_next_pages(5)
